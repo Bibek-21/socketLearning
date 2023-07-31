306d1f57bde9843507e3d679af4131de0b7ac100
Bibek Regmi <bibek.regmi@swifttech.com>
2023-07-31 16:34:51 +0545

    done until group message can be dome

--- public/main.js
+++ public/main.js
@@ -1,36 +1,60 @@
 // const socket = io("http:localhost:3000",{});
 
-const socket= io('http://localhost:8080',{})
+const socket = io('http://localhost:8080', {})
 
 
-const clientNumber= document.getElementById("client-total");
+const clientNumber = document.getElementById("client-total");
 const messageContainer = document.getElementById("message-container");
 const nameInput = document.getElementById("name-input");
 const messageForm = document.getElementById("message-form");
 const messageInput = document.getElementById("message-input");
 
-function sendMessage(){
-    console.log(messageInput.value);
-    const data = {
-        name: nameInput.value,
-        message:messageInput.value,
-        dateTime: new Date()
-    };
-    socket.emit('message',data);
+
+
+function addMessageToUI(isOwnMessage, data) {
+
+    const element = ` <li class="${isOwnMessage ? "message-right" : "message-left"}">
+        <p class="message">
+         ${data.message}
+        <span> ${data.name} ● ${data.dateTime}
+        </span>
+        </p>
+      </li>`
+
+    messageContainer.innerHTML += element;
+
 }
 
-messageForm.addEventListener('submit',(e)=>{
+
+function sendMessage() {
+    console.log(messageInput.value);
+
+    const data = {
+        name: nameInput.value,
+        message: messageInput.value,
+        dateTime: new Date().toDateString()
+    };
+
+    socket.emit('message', data);
+    addMessageToUI(true, data);
+    messageInput.value = ''
+
+}
+
+messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
     sendMessage();
 })
 
-socket.on('clients-total',(data)=>{
-    clientNumber.innerText= `Clients-Total:${data}`;
+socket.on('clients-total', (data) => {
+    clientNumber.innerText = `Clients-Total:${data}`;
 })
 
-socket.on('chatMessage',(data)=>{
-    console.log("i am here bro am i ?");
-    console.log(data);
+socket.on('chatMessage', (data) => {
+    // console.log("i am here bro am i ?");
+    // console.log(data);
+    addMessageToUI(false, data)
+
 })
 
 
